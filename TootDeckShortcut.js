ab16c1acc4234cea3dbc71d9d66355a4d4792f5f
TacosTea <tacos@tacostea.net>
2017-06-29 10:11:10 +0900

    Revert "ドロワの開閉に対応(カラム数が増えてきたので)"
    This reverts commit 24b1ea53cc95857bdc94ffc06f477c4cf3bbe927.

--- TootDeckShortcut.js
+++ TootDeckShortcut.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         TootDeck Shortcut
+// @name         MastodonDeck Shortcut
 // @namespace    http://github.com/TacosTea/
-// @version      0.6
+// @version      0.5
 // @description  Add shortcut keys like TweetDeck to Mastodon
 // @author       tacostea
 // @match        https://*/web/*
@@ -11,51 +11,21 @@
 
 (function() {
  'use strict';
-
- var drawer = document.getElementsByClassName("drawer")[0];
- function open_drawer(){
- drawer.style.width="300px";
- }
- function close_drawer(){
- drawer.style.width="0";
- }
-
- close_drawer();
-
- var elm = document.createElement('div');
- elm.id = ('drawer__nobb');
- elm.innerHTML='<button id="nobb">CLOSE(c)</button>';
- elm.style.margin="20px auto";
- document.getElementsByClassName("drawer__inner")[0].appendChild(elm);
-
- document.activeElement.blur();
-
  document.onkeyup = function(e) {
    if (document.activeElement.tagName != "INPUT" && document.activeElement.tagName != "TEXTAREA") {
-     switch (e.keyCode) {
+   switch (e.keyCode) {
+   case 78:
+   // Key: n
+   document.getElementsByClassName("autosuggest-textarea__textarea")[0].focus();
+   document.getElementsByClassName("autosuggest-textarea__textarea")[0].value="";
+   break;
 
-       case 67:
-         // key: c - Close drawer
-         close_drawer();
-         break;
-
-       case 78:
-         // Key: n - prepare to post New toot
-         open_drawer();
-         document.getElementsByClassName("autosuggest-textarea__textarea")[0].focus();
-         document.getElementsByClassName("autosuggest-textarea__textarea")[0].value="";
-         break;
-
-       case 83:
-         // key: s - prepare to Search
-         open_drawer();
-         document.getElementsByClassName("search__input")[0].focus();
-         document.getElementsByClassName("search__input")[0].value="";
-         break;
-     }
+   case 83:
+   // key: s
+   document.getElementsByClassName("search__input")[0].focus();
+   document.getElementsByClassName("input.search__input")[0].value="";
+   break;
    }
- };
-})();
-
-
-
+   }
+   };
+ })();
